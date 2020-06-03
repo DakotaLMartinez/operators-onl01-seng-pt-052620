d5ac5dec8b2adc9575c5f0dc0b7bf60bb578e1d6
Dakota Martinez <dakotaleemusic@gmail.com>
2020-06-03 21:10:06 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,29 @@
+require 'pry'
 def unsafe?(speed)
-
+  !(40..60).include?(speed)
+  speed < 40 || speed > 60
 end
 
 
 
 def not_safe?(speed)
-	
+  speed = speed.to_i
+	speed < 40 || speed > 60 ? true : false
 end
+
+def pluralize(word, number)
+    if number == 1 
+      "#{number} #{word}"
+    else 
+      "#{number} #{word}s"
+    end 
+    number == 1 ? "#{number} #{word}" : "#{number} #{word}s"
+end
+
+pluralize('pizza', 2)
+# => '2 pizzas'
+pluralize('pizza', 1)
+# => '1 pizza'
 	
 
-
+binding.pry
